50d2834af53751f3ac78330eb1976a2204630d98
Bz-Carter <lebz2016@gmail.com>
2021-09-15 19:05:48 +0000

    commit: migrations, models & controllers created.

---
+++ database/migrations/2021_09_13_084328_create_staff_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff_members', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('description');
+            $table->string('statu');
+            $table->string('photo');
+            $table->integer('staff_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff_members');
+    }
+}
---
+++ database/migrations/2021_09_13_084912_create_goals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->id();
+            $table->string('season_id');
+            $table->string('match_id');
+            $table->string('goal_sticker_id');
+            $table->string('goal_passer_id');
+            $table->string('goal_moment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goals');
+    }
+}
